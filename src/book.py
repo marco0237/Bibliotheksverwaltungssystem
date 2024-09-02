14739604f6cf32e56eba3bc902b9aafb8d2c4f17
Ghislain Z. <gzeleu@outlook.de>
2024-09-02 23:06:01 +0200

    Definition of Book and Member classes

--- src/book.py
+++ src/book.py
@@ -1,4 +1,15 @@
 
 
 class Book:
-    pass
+    
+    def __init__(self, title:str, author:str, isbn:str, available:bool) -> None:
+         # Attribute:
+        self.title = title
+        self.author = author
+        self.isbn = isbn    
+        self.available = available
+
+    def  __str__(self):
+        return f"This is a book named {self.title} and author = {self.author}"
+       
+ 
