5bf24f925c1542198485687d6ba7db934c517a11
marco <mcfansi@gmx.de>
2024-09-05 22:28:53 +0200

    Ausgabe

--- src/member.py
+++ src/member.py
@@ -1,33 +1,26 @@
 from typing import List # Typing concpet in python
 from book import Book
 
-
 class Member:    
     
-    def __init__(self, name:str, id:int, borrowed_books: List[Book]) -> None:
+    def __init__(self, name:str, member_id:int, borrowed_books: List[Book]) -> None:
          # Attribute:
         self.name = name
-        self.id = id
+        self.member_id = member_id
         self.borrowed_books = borrowed_books  
 
     # Methoden:
-    def borrow_book(self, book: Book) -> str:            
+    def borrow_book(self, book: Book):
         if book.available:
-                  for book_item in  self.borrowed_books:
-                     if book_item == book:
-                           book_item.available = False
-                           return "Ok"
-        else:
-            return "Book not exists"
+            self.borrowed_books.append(book)
                       
-                    
-                          
-                  
-           
-                  
-    
-    def return_book(self):
-            pass
+
+    def return_book(self, book: Book):
+        if any(book in item for item in self.borrowed_books):
+            self.borrowed_books.remove(book)
+            return True
+        else:
+            return False
     
 
     def __str__(self):
