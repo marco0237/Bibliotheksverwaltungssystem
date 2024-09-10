372fee9f03321b140667631149030639e36bf3a5
marco0237 <111447774+marco0237@users.noreply.github.com>
2024-09-11 00:06:20 +0200

    Klasse Library und Klasse Test_library  sowie  app für Kontroller aktualisiert  (#19)
    * Ausgabe
    
    * library und Test_library
    
    * library und Test_library
    
    * library und Test_library
    
    * library und Test_library
    
    * library und Test_library
    
    * library und Test_library

--- src/models/library.py
+++ src/models/library.py
@@ -1,39 +1,64 @@
-from typing import List  # Typing concept in python
-from models.book import Book
-from models.member import Member
-
+from src.models.book import Book
+#TDD(test drive developp)
 
 class Library:
     # Initialisierungsmethode Konstruktor
     def __init__(self):
-        # self.books = List[Book] # Public Attribut books
-        # self.members = List[Member] #Attribut members
+        #self.books = List[Book] # Public Attribut books
+        #self.members = List[Member] #Attribut members
         self.books = []  # Public Attribut books
         self.members = list()  # Attribut members
 
     # Methoden
-    def add_book(self, book: Book):
+    def add_book(self, book:Book):
         self.books.append(book)
+
 
     def remove_book(self, book):
         if book in self.books:
             self.books.remove(book)
-            print(f"Buch '{book.title}' wurde entfernt")
+            return True
+            #print(f"Buch '{book.title}' wurde entfernt")
         else:
-            print(f"buch'{book.title}' ist nicht in der Bibiothek")
+            return False
+            #print(f"buch'{book.title}' ist nicht in der Bibiothek")
+
 
     def register_member(self, member):
         self.members.append(member)
-        print(f"Der neuen Miglied heißt'{member.name}'")
+        return True
+        #print(f"Der neuen Miglied heißt'{member.name}'")
+
 
     def find_book_by_isbn(self, isbn):
         for book in self.books:
             if book.isbn == isbn:
                 return book
-        print(f"es existiert kein Buch mit'{isbn}'")
+            else:
+                return False
+        #print(f"es existiert kein Buch mit'{isbn}'")
 
     def list_available_books(self):
-        pass
+        #Auflisten alle verfügbare Bücher
+        available_books = []
+        for book in self.books:
+            if not book.available:
+                available_books.append(book)
+        if available_books:
+            for book in available_books:
+                return True
+        else:
+            return False
 
     def list_borrowed_books(self):
+        # Auflisten alle augeliehen Bücher
+        borrowed_books = []
+        for book in self.books:
+            if book.available:
+                borrowed_books.append(book)
+        if borrowed_books:
+            for book in borrowed_books:
+                return True
+        else:
+            return False
         pass
--- tests/models/units/test_library.py
+++ tests/models/units/test_library.py
@@ -1,8 +1,27 @@
 # test_library.py
-from unittest import TestCase
+from pathlib import Path
+import sys
 
+OUTPUT_PATH = Path(__file__).parents[3]
+SRC_PATH = OUTPUT_PATH / Path("./src")
+sys.path.append(str(SRC_PATH))
+
+from unittest import TestCase
+from src.models.library import Library
+from src.models.member import Member
+from src.models.book import Book
 
 class LibraryTestCase(TestCase):
 
     def test_create_library(self):
-        pass
+        self.library_1 = Library() # cree und Objet par rapport à __init__
+        self.assertEqual(len(self.library_1.books),0)
+        self.assertEqual(len(self.library_1.members), 0)
+
+    def test_add_book(self):
+        library_2 = Library()
+        book_1 = Book("Python 3","Johannes Ernesti", "987-3-8362-9129-3",True)
+        library_2.add_book(book_1)
+        self.assertEqual(1, len(library_2.books))
+
+
