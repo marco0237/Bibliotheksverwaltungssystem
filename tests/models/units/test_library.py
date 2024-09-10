f8a37532181973557933151f96ffc5528ddc5ddd
marco <mcfansi@gmx.de>
2024-09-10 23:35:05 +0200

    library und Test_library

--- tests/models/units/test_library.py
+++ tests/models/units/test_library.py
@@ -14,7 +14,6 @@ from src.models.library import Library
 from src.models.member import Member
 from src.models.book import Book
 
-
 class LibraryTestCase(TestCase):
 
     def test_create_library(self):
