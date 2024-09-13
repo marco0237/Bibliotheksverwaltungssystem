fc8a023028f7c01713ce8aeead719cb5539d4faa
marco <mcfansi@gmx.de>
2024-09-13 20:20:50 +0200

    update file_1309

--- src/models/library.py
+++ src/models/library.py
@@ -1,18 +1,18 @@
 from src.models.book import Book
-#TDD(test drive develop)
+# TDD(test drive develop)
+
 
 class Library:
     # initialise constructor
     def __init__(self):
-        #self.books = List[Book] # Public Attribute books
-        #self.members = List[Member] #Attribut members
+        # self.books = List[Book] # Public Attribute books
+        # self.members = List[Member] # Attribut members
         self.books = []  # Public Attribute books
         self.members = list()  # Attribute members
 
     # Methode
-    def add_book(self, book:Book):
+    def add_book(self, book: Book):
         self.books.append(book)
-
 
     def remove_book(self, book):
         if book in self.books:
@@ -21,11 +21,9 @@ class Library:
         else:
             return False
 
-
     def register_member(self, member):
         self.members.append(member)
         return True
-
 
     def find_book_by_isbn(self, isbn):
         for book in self.books:
@@ -35,13 +33,12 @@ class Library:
                 return False
 
     def list_available_books(self):
-        #Auflisten alle available book
+        # Auflisten alle available book
         available_books = []
         for book in self.books:
             if book.available:
                 available_books.append(book)
         return available_books
-
 
     def list_borrowed_books(self):
         borrowed_books = []
